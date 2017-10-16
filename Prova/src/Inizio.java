fa43c7dc7d60a9ce5bcca85a23ba9120c547767f
Alunno <Alunno@PC-11-INF4>
2017-10-16 12:22:02 +0200

    Modifica

--- Prova/src/Inizio.java
+++ Prova/src/Inizio.java
@@ -3,7 +3,7 @@ public class Inizio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word");
+		System.out.println("Hello Word 2");
 	}
 
 }
